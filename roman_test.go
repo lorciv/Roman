597bf88989478bcce0fe3f354893eb6f40c3e8ca
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2020-03-18 12:54:18 +0100

    table drived test

--- roman_test.go
+++ roman_test.go
@@ -1,20 +1,25 @@
 package roman
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("convert 1", func(t *testing.T) {
-		got := Ator(1)
-		want := "I"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-	t.Run("convert 2", func(t *testing.T) {
-		got := Ator(2)
-		want := "II"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	tests := []struct {
+		arabic int
+		roman  string
+	}{
+		{1, "I"},
+		{2, "II"},
+	}
+	for _, test := range tests {
+		descr := fmt.Sprintf("convert %d to %s", test.arabic, test.roman)
+		t.Run(descr, func(t *testing.T) {
+			got := Ator(test.arabic)
+			if got != test.roman {
+				t.Errorf("got %q, want %q", got, test.roman)
+			}
+		})
+	}
 }
